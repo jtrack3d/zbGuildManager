1a6777f5a372ccf7d7f2c37e193e5f5add2dd6a6
jtrack3d <jtrack3d@gmail.com>
2017-07-06 15:52:15 -0500

    initial

---
+++ Locale/enUS.lua
@@ -0,0 +1,33 @@
+﻿-- English localization file for enUS and enGB.
+local debug = false
+--[===[@debug@
+debug = true
+--@end-debug@]===]
+
+local L = LibStub("AceLocale-3.0"):NewLocale("zbGuildManager", "enUS", true, debug)
+
+-- here come the english Translations Strings
+L["# Alts"] = "# Alts";
+L["Are you sure you wish to remove selected players from the guild?"] = "Are you sure you wish to remove selected players from the guild?";
+L["Dissociate"] = "Dissociate";
+L["Export"] = "Export";
+L["Export Data"] = "Export Data";
+L["Joined"] = "Joined";
+L["Last On"] = "Last On";
+L["Mains/All"] = "Mains/All";  -- Toggle Mains or All
+L["Make %s an alt of %s?"] =  "Make %s an alt of %s?";
+L["Name"] = "Name";
+L["No"] = "No";
+L["Realm"] = "Realm";
+L["Remove"] = "Remove";
+L["Rank"] = "Rank";
+L["Search"] = "Search";
+L["Select All"] = "Select All";
+L["Set Alt"] = "Set Alt";
+L["Set Main"] = "Set Main";
+L["Set Main / Join Date"] = "Set Main / Join Date";
+L["Set Rank"] = "Set Rank";
+L["Total"] = "Total";
+L["Unassoc"] = "Unassoc";  -- Unassociated characters.  Not a main nor alt.
+L["Unassociated Characters"] = "Unassociated Characters";
+L["Yes"] = "Yes";
